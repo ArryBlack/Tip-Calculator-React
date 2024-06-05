9ee8b103d6c860a387d0427480cab32c4bead008
ArryBlack <sikdararyarishi.india@gmail.com>
2024-06-05 18:46:28 +0530

    add: deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Tip-Calculator-App-with-ReactJS-Frontend-Mentor/', 
+  base: '/Tip-Calculator-React/', 
 })
